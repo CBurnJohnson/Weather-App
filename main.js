93b695735285963dc5cec5abfc18c8cab7f76473
Cole Johnson <cburnjohnson@gmail.com>
2020-03-29 11:30:42 -0700

    added current location

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-window.addEventListener('load', async () => {
-    const res = await fetch(
-        `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b3ec4eba475177904e2f7fc2bc38ece6/37.8267,-122.4233`
-    );
+window.addEventListener('load', () => {
+    let long;
+    let lat;
 
-    const data = await res.json();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
 
-    const { summary, temperature, icon } = data.currently;
+            const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b3ec4eba475177904e2f7fc2bc38ece6/${lat},${long}`;
 
-    document.getElementById('timezone').innerHTML = data.timezone;
-    document.getElementById('summary').innerHTML = summary;
-    document.getElementById('temp').innerHTML = temperature;
+            const res = await fetch(api);
 
-    setIcon('icon', icon);
+            const data = await res.json();
+
+            const { summary, temperature, icon } = data.currently;
+
+            document.getElementById('timezone').innerHTML = data.timezone;
+            document.getElementById('summary').innerHTML = summary;
+            document.getElementById('temp').innerHTML = `${temperature}&#176;F`;
+
+            setIcon('icon', icon);
+        });
+    }
 });
 
 const setIcon = (iconId, icon) => {
